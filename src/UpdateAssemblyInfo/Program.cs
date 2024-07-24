01b526f841b4b531d2b2ed48df852560d72efe0b
jenna-aspecta <163962700+jenna-aspecta@users.noreply.github.com>
2024-07-24 16:23:25 +0800

    Update Program.cs

--- src/UpdateAssemblyInfo/Program.cs
+++ src/UpdateAssemblyInfo/Program.cs
@@ -6,6 +6,14 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml;
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
 
 namespace UpdateAssemblyInfo
 {
